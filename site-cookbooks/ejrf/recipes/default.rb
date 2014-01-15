c8c8c199fb34c1721187bf728897695931cf8284
Remo Samuel Paul <remosam42@gmail.com>
2014-01-15 16:46:35 +0300

    neil/remo finalizing chef changes

--- site-cookbooks/ejrf/recipes/default.rb
+++ site-cookbooks/ejrf/recipes/default.rb
@@ -9,30 +9,49 @@ package 'git' do
    action :install
 end
 
-#python dependencies
+package "locales" do
+  action :install
+end
+
+locale = "en_US.UTF-8"
+
+ruby_block "Set locale for this session" do
+    block do
+        ENV["LANG"] = locale 
+        ENV["LC_ALL"] = locale 
+    end
+end
+
+execute "Set locale permanently" do
+    command "update-locale LANG=#{locale} LC_ALL=#{locale}"
+end
+
+execute "Set timezone" do
+    command 'echo "Etc/UTC" > /etc/timezone'
+end
 
 execute 'update system' do
    command 'sudo apt-get update'
    action :run
 end
 
-%w{ build-essential python-dev python-pip python-setuptools python-pip libpq-dev libxml2 libxml2-dev libxslt1-dev }.each do |pkg|
+%w{ build-essential python-virtualenv python2.7-dev python-pip python-setuptools libpq-dev libxml2 libxml2-dev libxslt1-dev }.each do |pkg|
     package pkg do
 	action :install
     end
 end
-
 
 user node["user"]["name"] do
     action :create
     username node["user"]["name"]
     password node["user"]["password"]
     home "/home/#{node["user"]["name"]}"
+
 end
 
 directory "/home/#{node["user"]["name"]}/app" do
     owner node["user"]["name"]
-    mode 00644
+    mode 00755
     action :create
     recursive true
 end
@@ -46,19 +65,11 @@ end
 git "/home/#{node["user"]["name"]}/app" do
     repository node[:application][:repo]
     destination "/home/#{node["user"]["name"]}/app/src"
-    group node["user"]["name"]
     action :sync
 end
 
-directory "/home/#{node["user"]["name"]}/app/venv" do
-    owner node["user"]["name"]
-    mode 00644
-    action :create
-    recursive true
-end
-
 execute "setup virtualenv for application" do
-    command "virtualenv --no-site-packages /home/#{node["user"]["name"]}/app/venv"
+    command "virtualenv --no-site-packages /home/#{node["user"]["name"]}/app/venv/"
     action :run
 end
 
@@ -67,13 +78,21 @@ execute "install pip requirements" do
     command "bash -c 'source /home/#{node["user"]["name"]}/app/venv/bin/activate && pip install -r pip-requirements.txt'"
 end
 
-%w{libmemcached-dev  libsasl2-dev libcloog-ppl-dev libcloog-ppl0 }.each do |pkg|
+%w{libmemcached-dev libpq-dev postgresql-contrib-9.1 libsasl2-dev libcloog-ppl-dev libcloog-ppl0 }.each do |pkg|
 		package pkg
 end
 
-package 'postgresql' do
+package 'postgresql-9.1' do
   action :install
 end
+
+directory '/etc/postgresql/9.1/main' do
+   owner 'postgres'
+   group 'postgres'
+   mode 0700
+   recursive true
+   action :create
+ end
 
 template "/etc/postgresql/9.1/main/pg_hba.conf" do
   user "postgres"
@@ -85,44 +104,47 @@ template "/etc/postgresql/9.1/main/postgresql.conf" do
   source "postgresql.conf.erb"
 end
 
-service "postgresql" do
+directory '/var/lib/postgresql/9.1/main' do
+   owner 'postgres'
+   group 'postgres'
+   mode 0700
+   recursive true
+   action :create
+ end
+
+
+execute  "create postgres socket" do
+   command "touch /var/run/postgresql/.s.PGSQL.5432" 
+end 
+
+service 'postgresql' do
   action :restart
 end
 
-execute "create-root-user" do
-    code = <<-EOH
-    psql -h localhost -U postgres -c "select * from pg_user where usename='root'" | grep -c root
-    EOH
-    command "createuser -U postgres -h localhost -s root"
-    not_if code 
-end
- 
 execute "create-database-user" do
-    code = <<-EOH
-    psql -h localhost -U postgres -c "select * from pg_user where usename='#{node["user"]["name"]}'" | grep -c #{node["db"]["password"]}
-    EOH
-    command "createuser -U postgres -h localhost -sw ejrf"
+    code = "psql -U #{node["user"]["name"]} -c \"select * from pg_user where usename='#{node["user"]["name"]}'\" | grep -c #{node["user"]["name"]}"
+    command "createuser -h localhost -U postgres -sw #{node["user"]["name"]}"
     not_if code 
 end
 
 execute "create-database" do
-    exists = <<-EOH
-    psql -h localhost -U ejrf -c "select * from pg_user where usename='#{node["user"]["name"]}'" | grep -c #{node["db"]["password"]}
-    EOH
-    command "createdb -U ejrf -h localhost -O ejrf -E utf8 -T template0 #{node["user"]["name"]}"
+    exists = "psql -U #{node["user"]["name"]} -c \"select * from pg_user where usename='#{node["user"]["name"]}'\" | grep -c #{node["user"]["name"]}"
+    command "createdb -h localhost -U #{node["user"]["name"]} -O #{node["user"]["name"]} -E utf8 -T template0 #{node["user"]["name"]}"
     not_if exists
 end
 
 
 execute "syncdb " do
+    user "ejrf"
+    cwd "/home/#{node["user"]["name"]}/app/src/"
+    command "bash -c 'source /home/#{node["user"]["name"]}/app/venv/bin/activate && python manage.py syncdb --noinput '"
     action :run
-    command "bash -c 'source /home/#{node["user"]["name"]}/app/venv/bin/activate && python manage.py syncdb --noinput --settings=settings.prod'"
-    cwd "/home/#{node["user"]["name"]}/app/src/#{node["user"]["name"]}"
 end
 execute "migrate " do
+    user "ejrf"
+    cwd "/home/#{node["user"]["name"]}/app/src/"
+    command "bash -c 'source /home/#{node["user"]["name"]}/app/venv/bin/activate && python manage.py migrate '"
     action :run
-    command "bash -c 'source /home/#{node["user"]["name"]}/app/venv/bin/activate && python manage.py migrate --settings=settings.prod'"
-    cwd "/home/#{node["user"]["name"]}/app/src/#{node["user"]["name"]}"
 end
 
 package 'nginx' do
@@ -147,13 +169,13 @@ end
 
 template "/etc/uwsgi/apps-available/#{node["user"]["name"]}.ini" do
 	source 'wsgi_app_conf.ini.erb'
-  variables ({:app => node["app"]["name"],:venv => "/home/#{node["user"]["name"]}/app/venv",:project => "/home/#{node["user"]["name"]}/app/src/#{node["user"]["name"]}"})
+  variables ({:app => node["app"]["name"],:venv => "/home/#{node["user"]["name"]}/app/venv",:project => "/home/#{node["user"]["name"]}/app/src/"})
 end
 
 template "/etc/nginx/sites-available/#{node["user"]["name"]}" do
     action :create
     source "nginx-app.conf.erb"
-    variables ({:sock => "tmp/#{node["user"]["name"]}.sock",:app => "/home/#{node["user"]["name"]}/app/src/#{node["user"]["name"]}",:name => node["user"]["name"]})
+    variables ({:sock => "tmp/#{node["app"]["name"]}.sock",:app => "/home/#{node["user"]["name"]}/app/src/#{node["app"]["name"]}",:name => node["user"]["name"]})
 end
 
 link "/etc/uwsgi/apps-enabled/#{node["user"]["name"]}.ini" do
@@ -168,11 +190,18 @@ execute  "restart uwsgi" do
     action :run
     command "sudo service uwsgi restart"
 end
+
 execute  "touch app reload " do
     action :run
-    command "touch /home/#{node["user"]["name"]}/app/src/#{node["user"]["name"]}/reload"
+    command "touch /home/#{node["user"]["name"]}/app/src/#{node["app"]["name"]}/reload"
 end
 
-service 'uwsgi' do
-	action :start
+
+service 'nginx' do
+  action :restart
+end
+
+execute  "restart uwsgi" do
+    action :run
+    command "sudo service uwsgi restart"
 end
